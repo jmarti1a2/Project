71d540382731b5de614cd9a27308fc04ece42464
jmarti1a2 <jmarti1a2@gmail.com>
2021-10-30 03:49:03 -0500

    se crean los botones de la sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ImagenLogo from './ImagenLogo'
 
 const Sidebar = () => {
-    return <nav className='w-72 border-gray-300'>SideBar</nav> 
-}
+    return (
+    <nav className='w-72 border border-gray-300 h-full flex flex-col bg-gray-300 p-5'>
+        <Link to='/admin'>
+            <ImagenLogo/>
+         </Link>
+
+        <div className='my-4'>
+        <Ruta icono='fas fa-car' ruta='/admin/perfil' nombre='Perfil'/>
+        <Ruta icono='fas fa-car' ruta='/admin/productos' nombre='Productos'/>
+        <Ruta icono='fas fa-car' ruta='/admin/ventas' nombre='Ventas'/>
+        <Ruta icono='fas fa-car' ruta='/admin/usuarios' nombre='Usuarios'/>
+
+        </div>
+        <button>Cerrar sesión</button>  
+    </nav>
+
+    )}
+
+    const Ruta=({icono,ruta,nombre})=> {
+        return (
+            <Link to={ruta}>
+                <button className='p-1 my-3 bg-indigo-700 hover:bg-indigo-900 flex w-full items-center text-white rounded-md'>
+                    <i className={`${icono} w-10`} />
+                    {nombre}
+                </button>   
+            </Link>
+        )
+    }
 
 export default Sidebar
