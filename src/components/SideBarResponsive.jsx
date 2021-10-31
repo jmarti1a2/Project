30c1d5372800a9972344a6cceae6958345c987b4
jmarti1a2 <jmarti1a2@gmail.com>
2021-10-31 14:41:00 -0500

    se crea slidebar con navegación

---
+++ src/components/SideBarResponsive.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react/cjs/react.development'
+
+const SideBarResponsive = () => {
+    const [mostrarNavegacion,setMostrarNavegacion] = useState(false)
+    return (
+        <div className='md:hidden' 
+        onClick={()=>{setMostrarNavegacion(!mostrarNavegacion)
+        }}
+        >
+            <i className={`fas fa-${mostrarNavegacion?'times':'bars'} hover:text-blue-600`}/>
+        {mostrarNavegacion && (
+        <ul className='bg-gray-900 '>
+            <ResponsiveRoute nombre='perfil' ruta='/admin/perfil'/>
+            <ResponsiveRoute nombre='Productos' ruta='/admin/productos'/>
+            <ResponsiveRoute nombre='Ventas' ruta='/admin/ventas'/>
+            <ResponsiveRoute nombre='Usuarios' ruta='/admin/usuarios'/>
+            </ul>
+        )}
+        
+        </div>
+    )
+}
+
+const ResponsiveRoute = ({ruta,nombre}) =>{
+    return(
+        <Link to={ruta}>
+        <li className='text-gray-200' > {nombre}</li>
+        </Link>
+
+    )
+}
+
+export default SideBarResponsive
--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,19 +1,17 @@
 import Sidebar from 'components/Sidebar';
+import SideBarResponsive from 'components/SideBarResponsive';
 import React from 'react'
-
-
-
 
 
 const PrivateLayout = ({children }) => {
     return (
     <div className="flex w-screen h-screen">
-        <div className='flex flex-nowrap h-full w-full'>
+        <div className='flex flex-col md:flex-row flex-nowrap h-full w-full'>
             <Sidebar/>
-            <div className='md:hidden '>
-                <i className='fas fa-bars hover:text-yellow-600'/>
-            </div>   
-            <main className='flex w-full bg-blue-400 overflow-y-scroll items-center justify-center'> { children}</main>
+            <SideBarResponsive />
+            <main className='flex w-full bg-blue-400 overflow-y-scroll items-center justify-center'> 
+            { children}
+            </main>
         </div>
         
         
