586031e1717890fe618ce54bc6ef300eb1fba915
jmarti1a2 <jmarti1a2@gmail.com>
2021-10-30 21:34:13 -0500

    se adicionan iconos de font awesome

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,23 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import ImagenLogo from './ImagenLogo'
 
+
 const Sidebar = () => {
     return (
-    <nav className='w-72 border border-gray-300 h-full flex flex-col bg-gray-300 p-5'>
-        <Link to='/admin'>
-            <ImagenLogo/>
-         </Link>
+            <nav className='hidden md:flex md:w-72 border border-gray-300 h-full flex-col bg-gray-300 p-5'>            
+                <Link to='/admin'>
+                    <ImagenLogo />  
+                </Link>
 
-        <div className='my-4'>
-        <Ruta icono='fas fa-car' ruta='/admin/perfil' nombre='Perfil'/>
-        <Ruta icono='fas fa-car' ruta='/admin/productos' nombre='Productos'/>
-        <Ruta icono='fas fa-car' ruta='/admin/ventas' nombre='Ventas'/>
-        <Ruta icono='fas fa-car' ruta='/admin/usuarios' nombre='Usuarios'/>
-
-        </div>
-        <button>Cerrar sesión</button>  
-    </nav>
-
+                <div className='my-4'>
+                    <Ruta icono='fa fa-user' ruta='/admin/perfil' nombre='Perfil' />
+                    <Ruta icono='fa fa-car' ruta='/admin/productos' nombre='Productos' />
+                    <Ruta icono='fas fa-car' ruta='/admin/ventas' nombre='Ventas' />
+                    <Ruta icono='fas fa-car' ruta='/admin/usuarios' nombre='Usuarios' />
+                </div>
+                <button>Cerrar sesión</button>
+            </nav>
     )}
 
     const Ruta=({icono,ruta,nombre})=> {
--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,11 +1,18 @@
 import Sidebar from 'components/Sidebar';
 import React from 'react'
 
+
+
+
+
 const PrivateLayout = ({children }) => {
     return (
     <div className="flex w-screen h-screen">
         <div className='flex flex-nowrap h-full w-full'>
             <Sidebar/>
+            <div className='md:hidden '>
+                <i className='fas fa-bars hover:text-yellow-600'/>
+            </div>   
             <main className='flex w-full bg-blue-400 overflow-y-scroll items-center justify-center'> { children}</main>
         </div>
         
