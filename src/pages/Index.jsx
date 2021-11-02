44be16c7547406b3fb58f14ddb566114ddb111aa
ofigueroaduarte <ofigueroaduarte@gmail.com>
2021-11-02 12:20:46 -0500

    Inicio Ajuste Layout Publico-me falta alinear los objetos verticalmente

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
 function Index() {
-    return <div>contenido landing del negocio</div>
+    return <div className="h-full">
+        <div className='flex flex-row h-full justify-center'>
+            <div className="bg-white w-2/5 align-middle" > 
+
+                <div className='py-52'>
+                    <h1 className='m-3 text-center text-3xl font-extrabold text-gray-900' >
+                    Bienvenidos 
+                    </h1>
+                </div>           
+            </div>
+
+            <div className="bg-red-500 w-3/5">
+                <h1 className='m-3 text-center text-3xl font-extrabold text-gray-900' >
+                    Espacio para Imagen 
+                </h1>
+            </div>
+        </div>
+
+        
+        </div>
 }
 
 export default Index
