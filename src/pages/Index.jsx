7b6a832ed6059e82eb90008e24843d46c38fec4e
ofigueroaduarte <ofigueroaduarte@gmail.com>
2021-11-02 12:30:55 -0500

    Ajuste Items en public Layout

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,16 +3,19 @@ import React from 'react'
 function Index() {
     return <div className="h-full">
         <div className='flex flex-row h-full justify-center'>
-            <div className="bg-white w-2/5 align-middle" > 
+            <div className="bg-white w-2/5 flex flex-col justify-center text-center" > 
 
-                <div className='py-52'>
+                <div>
                     <h1 className='m-3 text-center text-3xl font-extrabold text-gray-900' >
                     Bienvenidos 
                     </h1>
-                </div>           
+                </div>  
+
+               
+
             </div>
 
-            <div className="bg-red-500 w-3/5">
+            <div className="bg-red-500 w-3/5 flex flex-col justify-center text-center">
                 <h1 className='m-3 text-center text-3xl font-extrabold text-gray-900' >
                     Espacio para Imagen 
                 </h1>
