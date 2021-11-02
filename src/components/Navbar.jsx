44a69a6382f7f56b4cecc864a0e47bd866ae1767
ofigueroaduarte <ofigueroaduarte@gmail.com>
2021-11-02 13:36:37 -0500

    Cambie boton cerrar sesión en Side-Bar agregando Icono y link al landing page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,20 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return <nav className='bg-purple-300'>
-        <ul className='flex w-full justify-between my-3'>
-            <li>
+        <ul className='flex w-full my-3'>
+            <li className= "w-4/5" >
             <img src={Logo} alt='imagen' className='Logo h-8 mx-4' />
-
             </li>
-            <li>Boton Nav1</li>
-            <li>Navegacion2</li>
-            <li>Navegacion3</li>
-            <li className='px-3'>
+
+            <li className='px-1 w-3/5 flex justify-end'>
+                <Link to='/Registro'>
+                    <button className='bg-indigo-400 p-2 text-white rounded-lg shadow-md hover:bg-indigo-800'>
+                        Registrarse
+                    </button>
+                </Link>
+            </li>
+
+            <li className='px-3 w-1/5 flex justify-end'>
                 <Link to='/login'>
                     <button className='bg-indigo-500 p-2 text-white rounded-lg shadow-md hover:bg-indigo-800'>
                         Iniciar Sesión
