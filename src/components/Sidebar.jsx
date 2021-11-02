438a62b075ddf7b4f4e64cdb704dfef39b72248f
jmarti1a2 <jmarti1a2@gmail.com>
2021-11-02 02:36:46 -0500

    se utiliza hook useLocation para mostar ruta activa

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { useEffect } from 'react/cjs/react.development'
 import ImagenLogo from './ImagenLogo'
 
 
@@ -21,9 +23,31 @@ const Sidebar = () => {
     )}
 
     const Ruta=({icono,ruta,nombre})=> {
+        const location = useLocation()
+        const [isActive, setIsActive]= useState(false)
+
+        useEffect(()=>{
+            console.log(location, ruta)
+            if(location.pathname.includes(ruta)){
+                setIsActive(true)
+            }
+            else{
+                setIsActive(false)
+            }
+        },[location, ruta])
+
+        useEffect(()=> {
+            console.log(isActive, ruta)
+        },[isActive,ruta])
+
+
         return (
             <Link to={ruta}>
-                <button className='p-1 my-3 bg-indigo-700 hover:bg-indigo-900 flex w-full items-center text-white rounded-md'>
+                <button
+                className={`p-1 my-3 bg-${
+                    isActive?'gray':'indigo'
+                    }-700 hover:bg-indigo-900 flex w-full items-center text-white rounded-md`} 
+                    >               
                     <i className={`${icono} w-10`} />
                     {nombre}
                 </button>   
