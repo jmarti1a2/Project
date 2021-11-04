eb396115f66135f59c2a1f67b45cc928ced5f32b
ofigueroaduarte <ofigueroaduarte@gmail.com>
2021-11-03 22:42:55 -0500

    Cambios esteticos interfaz Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Google from 'media/google.png';
 import { Link } from 'react-router-dom'
 
 function Login() {
@@ -25,15 +26,28 @@ function Login() {
                 <div className='flex-col justify-center items-center text-center py-6'>
                     <div className='py-1'>
                         <Link to='/admin'>
-                            <button type='submit'>Iniciar Sesión</button>
+                            <button type='submit' className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>Iniciar Sesión</button>
                         </Link>                   
                     </div>
-                    <div>
-                        o
+                                       
+                    <div className='flex items-center justify-center'>
+                            <span className='mx-4'>------------------------</span>
+                            <h2 className='my-4 text-center text-sm font-extrabold text-gray-900'>O</h2>
+                            <span className='mx-4'>------------------------</span>
                     </div>
-                    <div className='py-1'>
-                        <button>Continua con Google</button>
-                    </div>
+                    <div className='max-w-md w-full'>
+                          <div>
+                          <button
+                                type='submit'
+                                className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-gray-700 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500'
+                            >
+                                <div className='flex items-center justify-start'>
+                                <img src={Google} alt='Logo Google' className='h-6 w-6' />
+                                <span className='mx-4'>Continúa con Google</span>
+                                </div>
+                            </button>
+                            </div>
+                        </div>
                 </div>
             </form>
         </div>
