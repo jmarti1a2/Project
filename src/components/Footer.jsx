e60955fc71e2e5819e2008953ebe95d7f7f576f3
ofigueroaduarte <ofigueroaduarte@gmail.com>
2021-11-13 02:32:52 -0500

    Agregue iconos al Footer y quedo terminada la pagina de inicio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,13 @@ const Footer = () => {
     <div className="text-center flex flex-row md:flex justify-between">
         <div className=" w-2/5 flex flex-col justify-center text-center text-yellow-50">
         ¿Quienes Somos?
-               
+        
         </div>
         
-        <div className="w-3/5 flex flex-col justify-center text-center text-yellow-50" >Privacidad
+        <div className="w-3/5 flex flex-col justify-center text-center text-yellow-50" >
+        <p ><i class="fab fa-facebook">  Facebook</i></p>
+        <p ><i class="fab fa-instagram">  Instagram</i></p>
+        <p ><i class="fab fa-twitter">  Twitter</i></p>
         
         </div>
 
